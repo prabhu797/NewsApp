56631c8adea739c3b3c2a3fb87dde38c87dca764
prabhu797 <prabhudesai007@gmail.com>
2023-08-24 21:26:15 +0530

    Updated About component

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = (props) => {
     return (
       <div className='container my-3 text-center'>
         <Spinner mode={props.mode}/>
-        <h1 className='my-5'>Under progress... </h1>
+        <h1 className='my-5' style={{color: props.mode? 'white':'black'}}>Under progress... </h1>
         <img src="https://w7.pngwing.com/pngs/169/68/png-transparent-architectural-engineering-graphy-work-in-progress-angle-building-text-thumbnail.png" alt="" />
       </div>
     )
